d23cc795bcb8a18a3cf0efec62f735609987e9da
Tharinda Kodikara <tharindakodikara@outlook.com>
2024-05-19 19:11:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# homes-app
+ A web app created with Angular to buy housing properties
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  template: `
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        </header>
+      </a>
+    </main>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  `,
+  styleUrls: ['./app.component.css'],
+  imports: [HomeComponent, RouterModule]
+})
+export class AppComponent {
+  title = 'homes';
+}
